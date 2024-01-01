eb7acda42a4729a8280cf2bb81d4a6dd366cf91a
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-01 17:13:44 -0400

    Add a module that defines variables with type annotations

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -2,7 +2,14 @@
 """This is a module that defines variables with type annotations"""
 
 
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = "Holberton"
+a: int = 1
+"""a is an integer with a value of 1"""
+
+pi: float = 3.14
+"""pi is a float with a value of 3.14"""
+
+i_understand_annotations: bool = True
+"""i_understand_annotations is a boolean with a value of True"""
+
+school: str = "Holberton"
+"""school is a string with a value of Holberton"""
