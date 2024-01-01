c0c60e0a13fa8686b8c1a9e25f5800b68db2561f
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-01 17:05:36 -0400

    function that takes a float and returns a string representation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This module contains a function that takes a
+float n as argument and returns a string representation
+of the float"""
+
+
+def to_str(n: float) -> str:
+    """This function takes a float n as argument
+        and returns a string representation of the float"""
+    return str(n)
