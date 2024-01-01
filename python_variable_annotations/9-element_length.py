8584e68178b1641f0a93a16e791621cdf64a0b63
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-01 18:50:35 -0400

    Add function Element_length

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """ This module contains the function element_length """
 from typing import List, Tuple
 
+
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """
     Takes a list of strings as input and returns a list of tuples
