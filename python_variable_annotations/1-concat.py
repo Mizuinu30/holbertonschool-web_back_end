75f2fccc58aad3633a1d7f0a1e7869c4656a7315
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-01 16:44:28 -0400

    a function that contats two srtrings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!bin/usr/env python3
+"""This module contains a function that concatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concatenates two strings and returns the result"""
+    return str1 + str2
