a25b28245795fa96c79280e965b9c2ba123b5e75
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-02 10:59:31 -0400

    Add a coroutine that takes 2 args

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" This module contains a coroutine."""
+
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ This a coroutine that takes in two arguments."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for future in asyncio.as_completed(tasks):
+        delays = await future
+        delays.append(delays)
+    return delays
