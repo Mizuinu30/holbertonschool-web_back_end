c1ffb8660ac472ecf4ca80179b35e47091eb1520
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-02 15:15:14 -0400

    Add a function thta measures time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,16 +2,15 @@
 """This module contains a coroutine."""
 
 
+import time
 import asyncio
 from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    """This function measures the total execution time for wait n."""
-    tasks = asyncio.run(await_n(n, max_delay))
-    start = time.perf_counter()
-    asyncio.run(wait_n(n, max_delay))
-    end = time.perf_counter()
-    total_time = end - start
+async def measure_time(n: int, max_delay: int) -> float:
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    total_time = end_time - start_time
     return total_time / n
