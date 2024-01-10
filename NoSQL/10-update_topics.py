8ca82209a7ebe042741a7b6d69abca95b7640954
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-09 21:59:47 -0400

    10-update_topics.py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -2,23 +2,6 @@
 """Change school topics"""
 
 
-from pymongo import MongoClient
-
 def update_topics(mongo_collection, name, topics):
-    """
-    Update the topics of a school document based on the school name.
-
-    :param mongo_collection: pymongo collection object
-    :param name: String, the school name to update
-    :param topics: List of strings, the list of topics approached in the school
-    :return: None
-    """
-    if not name or not topics:
-        print("Name and topics must be provided.")
-        return
-
-    result = mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
-    if result.matched_count == 0:
-        print(f"No documents found with the name '{name}'.")
-    else:
-        print(f"Updated {result.matched_count} document(s).")
+    """changes all topics of a school document based on the name """
+    return mongo_collection.update_many({ "name": name }, { "$set": { "topics": topics } })
