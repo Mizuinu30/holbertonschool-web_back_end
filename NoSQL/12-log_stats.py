1b57a610394fee9454880cefcff384dc45a228a3
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-12 11:24:44 -0400

    fixed mongo client

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,7 +2,7 @@
 """Log stats"""
 from pymongo import MongoClient
 
-
+# Client
 client = MongoClient('mongodb://127.0.0.1:27017')
 
 # Database
@@ -11,12 +11,12 @@ collection = db.ngnix
 
 # Get total logs
 total_logs = collection.count_documents({})
-
+# Get methods
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 methods_count = {method: collection.count_documents({"method": method}) for method in methods}
-
+# Get status check
 status_counts = collection.count_documents({"method": "GET", "path": "/status"})
-
+# Print
 print(f"{total_logs} logs")
 print("Methods:")
 for methods in methods:
