2cbcc964a81cd1fb5e310ff2e3471af87b027f05
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-15 18:09:18 -0400

    Add const

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -8,7 +8,7 @@ export function taskFirst() {
   }
 
   export function taskNext() {
-    lety combination = 'But sometimes let';
+    let combination = 'But sometimes let';
     combination += getLast();
 
     return combination;
