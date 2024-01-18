a91956f7df160c038f5945cdb111aa371f7b3761
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-17 21:45:34 -0400

    9-getFullBudget.js

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,4 +1,3 @@
-/* eslint import/extensions: ['error', 'ignorePackages', { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' }] */ // eslint-disable-line
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
