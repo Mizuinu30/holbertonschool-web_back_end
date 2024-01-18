62f08712ef9b8a36e621b7a1b1999cccb1cf154e
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-17 21:40:46 -0400

    9-getFullBudget.js

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,16 +1,17 @@
+/* eslint import/extensions: ['error', 'ignorePackages', { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' }] */ // eslint-disable-line
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
+const budget = getBudgetObject(income, gdp, capita);
+const fullBudget = {
+  ...budget,
+  getIncomeInDollars(income) {
+    return `$${income}`;
+  },
+  getIncomeInEuros(income) {
+    return `${income} euros`;
+  },
+};
 
-  return fullBudget;
+return fullBudget;
 }
