6d0697ebf8221cbd008cbaed80480ef2662093ba
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-18 21:43:27 -0400

    fix lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -23,7 +23,7 @@ export default class HolbertonCourse {
     return this._length;
   }
 
- // Setter for length
+  // Setter for length
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
@@ -38,7 +38,7 @@ export default class HolbertonCourse {
 
   // Setter for students
   set students(newStudents) {
-    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = newStudents;
