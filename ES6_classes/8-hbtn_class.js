0db2cf8727e79a68d9704f9fccaad40e197776ef
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-19 16:14:24 -0400

    8-hbtn_class.js

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,33 +1,33 @@
 // 8-hbtn_class.js
 export default class HolbertonClass {
-    constructor(size, location) {
-      this._size = size;
-      this._location = location;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-    get size() {
+  get size() {
+    return this._size;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(newLocation) {
+    this._location = newLocation;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
       return this._size;
     }
-
-    set size(newSize) {
-      this._size = newSize;
-    }
-
-    get location() {
+    if (hint === 'string') {
       return this._location;
     }
-
-    set location(newLocation) {
-      this._location = newLocation;
-    }
-
-    [Symbol.toPrimitive](hint) {
-      if (hint === 'number') {
-        return this._size;
-      }
-      if (hint === 'string') {
-        return this._location;
-      }
-      return null;
-    }
+    return null;
   }
+}
