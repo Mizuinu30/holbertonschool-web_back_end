5cbbd5491c78e7753fce24bd66d8c06d00e2091c
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-19 09:30:44 -0400

    new

--- ES6_basic/12-createReportObject.js
+++
@@ -1,8 +0,0 @@
-export default function createReportObject(employeesList) {
-  return {
-    allEmployees: { ...employeesList }, // Spread syntax to ensure a copy, if needed
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
-  };
-}
