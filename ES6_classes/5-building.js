606b2a6a990544478f6add66aee6d4af9b2c2f53
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-19 13:42:48 -0400

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,13 @@
 class Building {
-    constructor(sqft){
+    constructor(sqft) {
         this._sqft = sqft;
     }
-    get sqft(){
+
+    get sqft() {
         return this._sqft;
     }
 
-    evacuationWarningMessage(){
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
     }
 }
