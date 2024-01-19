fa568e42f394d48173560919ab9a315fa31236e6
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-18 22:31:52 -0400

    3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,36 +1,36 @@
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
- }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+}
 
-    // Getter for code
-    get code() {
-    return this._code;
-    }
+  // Getter for code
+  get code() {
+  return this._code;
+  }
 
-    // Setter for code
-    set code(newCode) {
-      if (typeof newCode !== 'string'){
-        throw new TypeError('Code must be a string');
-    }
-    this._code = newCode;
-    }
+  // Setter for code
+  set code(newCode) {
+    if (typeof newCode !== 'string'){
+      throw new TypeError('Code must be a string');
+  }
+  this._code = newCode;
+  }
 
-    // Getter for name
-    get name() {
-        return this._name;
-    }
+  // Getter for name
+  get name() {
+      return this._name;
+  }
 
-    // Setter for name
-    set name(newName) {
-        if (typeof newName !== 'string'){
-            throw new TypeError('Name must be a string');
-        }
-        this._name = newName;
-    }
+  // Setter for name
+  set name(newName) {
+      if (typeof newName !== 'string'){
+          throw new TypeError('Name must be a string');
+      }
+      this._name = newName;
+  }
 
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
-    }
+  displayFullCurrency() {
+      return `${this.name} (${this.code})`;
+  }
 }
