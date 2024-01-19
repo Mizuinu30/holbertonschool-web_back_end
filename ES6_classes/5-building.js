11cf0d67ef37870ae6f890d8d054ca89d1d35f01
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-19 14:18:11 -0400

    5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,13 @@
+/* eslint-disable */
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Building cannot be instantiated directly');
-    }
-
-    this._sqft = sqft;
-
-    // Check if evacuationWarningMessage method is implemented in subclass
-    if (this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+      if (this.constructor !== Building && !this.evacuationWarningMessage) {
+          throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+      this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
-    return this._sqft;
+      return this._sqft;
   }
 }
