b69ddc45e6f031f8ecccae8212a1564ede7eba0d
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-19 21:42:18 -0400

    100-evcar.js

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,13 +1,29 @@
-import Car from './10-car;
+import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
+    // call constructor of super class (Building)
     super(brand, motor, color);
+
+    // Create objs
     this._range = range;
   }
 
+  // Methods
+
   cloneCar() {
-    // Clone the EVCar including its specific property _range
-    return new this.constructor(this._brand, this._motor, this._color, this._range);
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
   }
+
+  // Setters
+
+  // Getters
 }
+/* class TestCar extends Car {};
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
+console.log(tc1 instanceof TestCar);
+console.log(tc2 instanceof TestCar);
+console.log(tc1 == tc2); */
