568bd496af7abca13f1f8156b329b3553e1f5148
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-26 13:02:35 -0400

    9-try.js

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,15 +1,17 @@
-function guardrail(mathFunction) {
+export default function guardrail(mathFunction) {
   const queue = []; // initialize an empty array
 
   try {
-    // try to Execute the mathFunction and push the result to the queue
+    // try to execute the mathFunction and push the result to the queue
     const result = mathFunction();
+    queue.push(result);
   } catch (error) {
-    // if an error is caught, push the error to the queue
+    // if an error is caught, push the error message to the queue
     queue.push(`Error: ${error.message}`);
   } finally {
-    // push the length of the queue to the queue
+    // push the message 'Guardrail was processed' to the queue
     queue.push('Guardrail was processed');
   }
+
   return queue;
 }
