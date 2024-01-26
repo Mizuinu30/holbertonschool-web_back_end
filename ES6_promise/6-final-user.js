eb2ed589bba4a2198452e052a259c989094793c4
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-26 10:33:08 -0400

    hadle error for gerard

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstname, lastname, filename) {
   return Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
     .then((value) => ({ status: 'success', value }), (error) => ({ status: 'rejected', error }, console.log('Signup system offline'), console.log(error))
-    .finally(() => console.log('Got a response from the API')))
-    .catch((error) => ({ status: 'rejected', error }, console.log('Signup system offline'), console.log(error)));
+      .finally(() => console.log('Got a response from the API')))
+    .catch((error) => ({ status: 'rejected', error })(console.log('Signup system offline'), console.log(error)));
 }
