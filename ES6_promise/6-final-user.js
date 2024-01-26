22f7a27b6c927361b89cdb3a3e2943e45f5814de
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-26 08:05:28 -0400

    6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+// Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(results => {
+      return results.map(result => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+      }));
+    });
+}
