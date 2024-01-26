b96162b7dc6189819b281e4eb1077bfb83dcccca
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-26 06:57:16 -0400

    1-promise.js

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      // Resolve the promise with an object containing status and body
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      // Reject the promise with an error object containing a message
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
