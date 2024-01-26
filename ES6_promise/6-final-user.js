c29678f7106d2ea7253d41508edc366d93aa9eba
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-26 09:42:55 -0400

    fix lint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,7 @@
-// Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(results => {
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      }));
-    });
+export default function handleProfileSignup(firstname, lastname, filename) {
+  return Promise.all([uploadPhoto(filename), signUpUser(firstname, lastname)])
+    .then((value) => ({ status: 'success', value }), (error) => ({ status: 'error', error }));
 }
