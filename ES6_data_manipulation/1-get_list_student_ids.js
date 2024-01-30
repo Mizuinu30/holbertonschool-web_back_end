cac9348210569dd8aa94ae6041ac7f459e556fa8
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-30 19:18:51 -0400

    1-get_list_student_ids.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,3 +1,5 @@
+// Objective: Write a function that returns an array of ids from a list of objects.
+
 export default function getListStudents(array){
   if (!Array.isArray(array)) {
     return [];
