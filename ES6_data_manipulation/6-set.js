1a0d9a459b0de09825aadd8f1559598cfd0de977
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-30 20:10:24 -0400

    6-set.js

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,3 +1,6 @@
-function setFromArray(arr) {
-  return new Set(arr);
-}
+// A function named setFromArray that returns a Set from an array.
+
+export default function setFromArray(array) {
+    const set = new Set(array);
+    return set;
+  }
