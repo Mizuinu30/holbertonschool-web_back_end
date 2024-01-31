f640b7ef7ba8ae634778b0aece218ee0937802c2
Mizuinu30 <juansilva.dvm@gmail.com>
2024-01-30 20:33:24 -0400

    8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (startString === ''
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
+  const str = [];
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) {
+      str.push(element.slice(startString.length));
+    }
+  });
+  return str.join('-');
+}
